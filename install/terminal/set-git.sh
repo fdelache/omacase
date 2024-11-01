55085bc4708ab06f4e8a24ec1db78a2f1bd9b129
Franck Delache <franck.delache@shopify.com>
2024-11-01 16:14:08 -0400

    Ensure links are always made to defaults, and not configs

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -1,5 +1,5 @@
 # Configure git to use omacase config file.
-if ! git config --global --get-all include.path | grep -qF ~/.local/share/omacase/configs/git_config; then
+if ! git config --global --get-all include.path | grep -qF ~/.local/share/omacase/defaults/git_config; then
     # If not found, add the omacase path to the global git config
-    git config set --append --global include.path ~/.local/share/omacase/configs/git_config
+    git config set --append --global include.path ~/.local/share/omacase/defaults/git_config
 fi
