eb7e80ba8adfa64a6916776355059764872f8c9f
Franck Delache <franck.delache@shopify.com>
2024-11-01 11:14:45 -0400

    Fix Kitty configuration

--- install/desktop/app-kitty.sh
+++ install/desktop/app-kitty.sh
@@ -2,6 +2,7 @@ brew install kitty
 
 mkdir -p ~/.config/kitty
 cp ~/.local/share/omacase/configs/kitty.conf ~/.config/kitty/kitty.conf
+cp ~/.local/share/omacase/configs/kitty/zellij.session ~/.config/kitty/zellij.session
 cp ~/.local/share/omacase/themes/tokyo-night/kitty.conf ~/.config/kitty/theme.conf
 cp ~/.local/share/omacase/configs/kitty/fonts/CaskaydiaMono.conf ~/.config/kitty/font.conf
 cp ~/.local/share/omacase/configs/kitty/font-size.conf ~/.config/kitty/font-size.conf
