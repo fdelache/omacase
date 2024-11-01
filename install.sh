8453e562c39bdf5008c24445be1fd9fe958a225a
Franck Delache <franck.delache@shopify.com>
2024-11-01 12:56:06 -0400

    Place he gum installer in a required folder.

--- install.sh
+++ install.sh
@@ -5,12 +5,9 @@ set -e
 source ~/.local/share/omacase/install/check-version.sh
 
 echo "Get ready to make a few choices"
-source ~/.local/share/omacase/install/terminal/app-gum.sh >/dev/null
+source ~/.local/share/omacase/install/terminal/requried/app-gum.sh >/dev/null
 source ~/.local/share/omacase/install/first-run-choices.sh
 
 echo "Installing terminal and desktop tools..."
-
-# Install terminal tools
 source ~/.local/share/omacase/install/terminal.sh
-
 source ~/.local/share/omacase/install/desktop.sh
