315fff6f78b0aa86786d69ec3cd0e25e82b41e6d
Franck Delache <franck.delache@shopify.com>
2024-11-01 11:39:15 -0400

    Use default Kitty theme handling

--- bin/omacase-sub/theme.sh
+++ bin/omacase-sub/theme.sh
@@ -6,7 +6,7 @@ THEME=$(gum choose "${THEME_NAMES[@]}" "<< Back" --header "Choose your theme" --
 
 # Check if THEME is not empty and not "<< Back"
 if [[ -n "$THEME" && "$THEME" != "<<-back" ]]; then
-  cp "$OMACASE_PATH/themes/$THEME/kitty.conf" ~/.config/kitty/theme.conf
+  kitten themes --reload-in=all $THEME
   cp "$OMACASE_PATH/themes/$THEME/zellij.kdl" ~/Library/Application\ Support/org.Zellij-Contributors.Zellij/themes/$THEME.kdl
   sed -i .bak "s/theme \".*\"/theme \"$THEME\"/g" ~/Library/Application\ Support/org.Zellij-Contributors.Zellij/config.kdl
   cp "$OMACASE_PATH/themes/$THEME/neovim.lua" ~/.config/nvim/lua/plugins/theme.lua
--- install/desktop/app-kitty.sh
+++ install/desktop/app-kitty.sh
@@ -3,6 +3,7 @@ brew install kitty
 mkdir -p ~/.config/kitty
 cp ~/.local/share/omacase/configs/kitty.conf ~/.config/kitty/kitty.conf
 cp ~/.local/share/omacase/configs/kitty/zellij.session ~/.config/kitty/zellij.session
-cp ~/.local/share/omacase/themes/tokyo-night/kitty.conf ~/.config/kitty/theme.conf
 cp ~/.local/share/omacase/configs/kitty/fonts/CaskaydiaMono.conf ~/.config/kitty/font.conf
 cp ~/.local/share/omacase/configs/kitty/font-size.conf ~/.config/kitty/font-size.conf
+
+kitten themes --reload-in=all Tokyo Night
