8a47a4a3d2d4d32f81d0ebd6c3a4280f6f84d958
Franck Delache <franck.delache@shopify.com>
2024-11-01 12:44:34 -0400

    Add support to optionally Install Rubymine

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ set -e
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/omacase/install/check-version.sh
 
+echo "Get ready to make a few choices"
+source ~/.local/share/omacase/install/terminal/app-gum.sh >/dev/null
+source ~/.local/share/omacase/install/first-run-choices.sh
+
 echo "Installing terminal and desktop tools..."
 
 # Install terminal tools
---
+++ install/desktop/select-optional-apps.sh
@@ -0,0 +1,9 @@
+if [[ -v OMACASE_FIRST_RUN_OPTIONAL_APPS ]]; then
+	apps=$OMACASE_FIRST_RUN_OPTIONAL_APPS
+
+	if [[ -n "$apps" ]]; then
+		for app in $apps; do
+			source "$OMACASE_PATH/install/desktop/optional/app-${app,,}.sh"
+		done
+	fi
+fi
