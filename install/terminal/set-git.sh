ca88c8167be70041df8f4b141d93166d3c32b107
Franck Delache <franck.delache@shopify.com>
2024-11-01 14:06:22 -0400

    Ensure the include path is set only once for git.

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -1,3 +1,3 @@
 # Configure git to use omacase config file.
-git config --global include.path ~/.local/share/omacase/configs/git_config
+git config --global --replace-all include.path ~/.local/share/omacase/configs/git_config
 
