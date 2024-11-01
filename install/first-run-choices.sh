777304f676e24ef7fd164d8b522ea99ba4e1fcb2
Franck Delache <franck.delache@shopify.com>
2024-11-01 12:45:17 -0400

    Select Rubymine by default

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,3 +1,3 @@
 OPTIONAL_APPS=("Rubymine")
-DEFAULT_OPTIONAL_APPS=''
+DEFAULT_OPTIONAL_APPS='Rubymine'
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
