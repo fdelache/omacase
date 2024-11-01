6e96bf2764bffbe7baad5f44ebe47f8f0cfaf202
Franck Delache <franck.delache@shopify.com>
2024-11-01 12:52:45 -0400

    Fix script with zsh syntax

--- install/desktop/select-optional-apps.sh
+++ install/desktop/select-optional-apps.sh
@@ -2,8 +2,8 @@ if [[ -v OMACASE_FIRST_RUN_OPTIONAL_APPS ]]; then
 	apps=$OMACASE_FIRST_RUN_OPTIONAL_APPS
 
 	if [[ -n "$apps" ]]; then
-		for app in $apps; do
-			source "$OMACASE_PATH/install/desktop/optional/app-${app,,}.sh"
+		for app in ${(f)apps}; do
+			source "$OMACASE_PATH/install/desktop/optional/app-${(L)app}.sh"
 		done
 	fi
 fi
