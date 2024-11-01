63fc539a294c0d4b1534bc243fa3ce2ba1ce5ed4
Franck Delache <franck.delache@shopify.com>
2024-11-01 14:35:01 -0400

    Ensure git config does not alter existing include.path

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -1,3 +1,5 @@
 # Configure git to use omacase config file.
-git config --global --replace-all include.path ~/.local/share/omacase/configs/git_config
-
+if ! git config --global --get-all include.path | grep -qF ~/.local/share/omacase/configs/git_config; then
+    # If not found, add the omacase path to the global git config
+    git config set --append --global include.path ~/.local/share/omacase/configs/git_config
+fi
