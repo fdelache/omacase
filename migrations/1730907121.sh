d414c37ebdda9852540883bd8ea65b3f05076310
Franck Delache <franck.delache@shopify.com>
2024-11-06 10:37:44 -0500

    Add migration script

---
+++ migrations/1730907121.sh
@@ -0,0 +1,9 @@
+# Install gh package
+source $OMACASE_PATH/install/terminal/app-gh.sh
+
+# Install go
+brew install go
+
+# configure VSCode keybindings
+cp $OMACASE_PATH/configs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
