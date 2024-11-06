010f889aa91726299b64ee3f088f12363e67d9c1
Franck Delache <franck.delache@shopify.com>
2024-11-06 10:20:44 -0500

    Add some keybindings changes for VSCode

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -1,7 +1,8 @@
 brew install --cask visual-studio-code
 
 mkdir -p ~/Library/Application\ Support/Code/User
-cp ~/.local/share/omacase/configs/vscode.json ~/Library/Application\ Support/Code/User/settings.json
+cp ~/.local/share/omacase/configs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+cp ~/.local/share/omacase/configs/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 # Install default supported themes
 code --install-extension enkia.tokyo-night
